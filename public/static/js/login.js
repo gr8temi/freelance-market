ec8d45b1ee9239c0d5221a5b041552517ac4d830
Adams Temidire A <adamstemii@gmail.com>
2019-06-19 23:13:28 +0100

    Modify login logic

--- public/static/js/login.js
+++ public/static/js/login.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     
-    $("#login-form").submit(function(e){
+    $("#login-form").click(function(e){
         e.preventDefault();
-        var email = $("#email").val()
-        var password = $("#password").val()
+        var email = $("#log-email").val()
+        var password = $("#log-password").val()
         $.ajax({
-        url:"http://localhost:3000/user",
+        url:"http://localhost:3000/user?email="+email+"&password="+password,
         dataType:'json',
         type:'GET',
         data:{
@@ -15,23 +15,35 @@ $(document).ready(function(){
             // contentType:'application/json',
              
         success:function(data){
-        for(var i=0; i<data.length;i++){
-            if(email==data[i].email && password==data[i].password){
-                var id =data[i].id;
-                location.assign('http://localhost:3000/dashboard.html?'+id)
+            console.log(data.length)
+            if(data.length==0){
+                location.assign('http://localhost:3000/signup.html')
             }
             else{
-                // alert("Your account does not exist. Reverting you to Sign Up") 
-                location.assign('http://localhost:3000/signup.html')
-    
+             $.each(data, function(i,info){
+                
+                location.assign('http://localhost:3000/dashboard.html?'+info.id)
+            })   
             }
-        }
+            
+            
+        // for(var i=0; i<data.length;i++){
+        //     if(email==data[i].email && password==data[i].password){
+        //         var id =data[i].id;
+        //        
+        //     }
+        //     else{
+        //         // alert("Your account does not exist. Reverting you to Sign Up") 
+        //         location.assign('http://localhost:3000/signup.html')
+    
+        //     }
+        // }
 
     //  windows.location.assign('http://localhost:3000/dashboard.html/'+firstname)
         // alert(data)
         },
-              error:function(errorThrown){
-                 console.log(errorThrown)
+              error:function(){
+                location.assign('http://localhost:3000/signup.html')
               }
  
  
